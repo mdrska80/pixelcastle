041357ee3b6585d37da7f299dc235dc5401ae0f1
Kitty <martin.drska@gmail.com>
2013-09-05 16:07:07 +0200

    FPS test zobrazen na obrazovce

--- Views/interfaceView.cs
+++ Views/interfaceView.cs
@@ -8,6 +8,7 @@ namespace Castles
     public class interfaceView : Drawable
     {
         private SpriteAnimatedEx Sprite { get; set; }
+        private Text txtFPS { get; set; }
 
         public void init()
         {
@@ -15,6 +16,9 @@ namespace Castles
             Sprite = new SpriteAnimatedEx();
             Sprite.AddAnimatedSprite("MAIN", saCursor);
             Sprite.CurrentAnimation = "MAIN";
+
+            txtFPS = new Text();
+            txtFPS.Font = new Font("data/sansation.ttf");
         }
 
         public void Draw(RenderTarget target, RenderStates states)
@@ -25,8 +29,14 @@ namespace Castles
                 init();
 
             Sprite.CurrentSprite.Position = new Vector2f(v2i.X, v2i.Y);
+            txtFPS.DisplayedString = string.Format("FPS:  "+FPS.Value.ToString());
+            txtFPS.Position = new Vector2f(100,100);
+            txtFPS.Color = Color.Black;
+
+            target.Draw(txtFPS);
 
             Sprite.CurrentSprite.Update(FPS.SemiDelta);
+
         }
     }
 }
